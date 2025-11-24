332ba407a23803a0e6e662a4fd049aea5c6b75f0
justneit <chanhtiennguyen2512004@gmail.com>
2025-11-24 20:58:53 +0700

    setup core config

---
+++ lib/core/config/app_styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppStyles {
+  static const title = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.text,
+  );
+  static const subtitle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.text,
+  );
+
+  static const body = TextStyle(
+    fontSize: 17,
+    color: AppColors.text,
+  );
+
+  static const subbody = TextStyle(
+    fontSize: 14,
+    color: AppColors.text,
+  );
+
+  static const small = TextStyle(
+    fontSize: 13,
+    color: AppColors.text,
+  );
+}
